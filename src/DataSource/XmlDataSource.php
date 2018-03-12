05ba259d87fe5753401d0efe36244dc6d5b91cc4
belgattitude <belgattitude@gmail.com>
2018-03-12 17:15:13 +0100

    Added XMLDataSource

---
+++ src/DataSource/XmlDataSource.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Jasper report integration for PHP
+ *
+ * @link      https://github.com/belgattitude/soluble-jasper
+ * @author    Vanvelthem Sébastien
+ * @copyright Copyright (c) 2017 Vanvelthem Sébastien
+ * @license   MIT
+ */
+
+namespace Soluble\Jasper\DataSource;
+
+use Soluble\Jasper\DataSource\Contract\JRDataSourceFromReportParamsInterface;
+use Soluble\Jasper\DataSource\Contract\JRDataSourceFromReportParamsTrait;
+use Soluble\Jasper\DataSource\Contract\ReportParametrableInterface;
+use Soluble\Jasper\ReportParams;
+
+class XmlDataSource implements JRDataSourceFromReportParamsInterface, ReportParametrableInterface
+{
+    use JRDataSourceFromReportParamsTrait;
+
+    public const PARAM_XML_SOURCE         = 'net.sf.jasperreports.xml.source';
+    public const PARAM_XML_DATE_PATTERN   = 'net.sf.jasperreports.xml.date.pattern';
+    public const PARAM_XML_NUMBER_PATTERN = 'net.sf.jasperreports.xml.number.pattern';
+    public const PARAM_XML_LOCALE_CODE    = 'net.sf.jasperreports.xml.locale.code';
+    public const PARAM_XML_TIMEZONE_ID    = 'net.sf.jasperreports.xml.timezone.id';
+
+    public const DEFAULT_DATE_PATTERN   = 'yyyy-MM-dd';
+    public const DEFAULT_NUMBER_PATTERN = '0.####';
+    public const DEFAULT_LOCALE_CODE    = 'en_US';
+    public const DEFAULT_TIMEZONE_ID    = 'UTC';
+
+    /**
+     * @var string
+     */
+    private $xmlSource;
+
+    /**
+     * @var array
+     */
+    private $options;
+
+    /**
+     * @param string $xmlSource   file or url for the json source
+     * @param string $datePattern json date pattern in java style (i.e. 'yyyy-MM-dd')
+     */
+    public function __construct(
+        string $xmlSource,
+        string $datePattern = self::DEFAULT_DATE_PATTERN,
+        string $numberPattern = self::DEFAULT_NUMBER_PATTERN,
+        string $timezoneId = self::DEFAULT_TIMEZONE_ID,
+        string $localeCode = self::DEFAULT_LOCALE_CODE
+    ) {
+        $this->xmlSource = $xmlSource;
+        $this->setOptions([
+                self::PARAM_XML_SOURCE         => $xmlSource,
+                self::PARAM_XML_DATE_PATTERN   => $datePattern,
+                self::PARAM_XML_NUMBER_PATTERN => $numberPattern,
+                self::PARAM_XML_TIMEZONE_ID    => $timezoneId,
+                self::PARAM_XML_LOCALE_CODE    => $localeCode
+        ]);
+    }
+
+    public function setOptions(array $options)
+    {
+        $options[self::PARAM_XML_SOURCE]  = $this->xmlSource;
+        $this->options                    = $options;
+    }
+
+    public function getOptions(): array
+    {
+        return $this->options;
+    }
+
+    public function getDataSourceReportParams(): ReportParams
+    {
+        return new ReportParams($this->options);
+    }
+}
---
+++ tests/functional/XmlReportGenerationTest.php
@@ -0,0 +1,97 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Jasper report integration for PHP
+ *
+ * @link      https://github.com/belgattitude/soluble-jasper
+ * @author    Vanvelthem Sébastien
+ * @copyright Copyright (c) 2017 Vanvelthem Sébastien
+ * @license   MIT
+ */
+
+namespace JasperTest\Functional;
+
+use JasperTest\Util\PDFUtils;
+use PHPUnit\Framework\TestCase;
+use Soluble\Japha\Bridge\Adapter as BridgeAdapter;
+use Soluble\Jasper\DataSource\XmlDataSource;
+use Soluble\Jasper\Exception\JavaProxiedException;
+use Soluble\Jasper\Report;
+use Soluble\Jasper\ReportParams;
+use Soluble\Jasper\ReportRunnerFactory;
+
+class XmlReportGenerationTest extends TestCase
+{
+    /**
+     * @var BridgeAdapter
+     */
+    protected $ba;
+
+    public function setUp(): void
+    {
+        $this->ba = \JasperTestsFactories::getJavaBridgeAdapter();
+    }
+
+    /**
+     * @dataProvider xmlSourceProvider
+     */
+    public function testXmlReport(string $xmlSource): void
+    {
+        $reportFile = \JasperTestsFactories::getReportBaseDir() . '/13_report_xml_northwind.jrxml';
+
+        $jsonDataSource = new XmlDataSource($xmlSource);
+        $jsonDataSource->setOptions([
+            XmlDataSource::PARAM_XML_DATE_PATTERN   => 'yyyy-MM-dd',
+            XmlDataSource::PARAM_XML_NUMBER_PATTERN => '#,##0.##',
+            XmlDataSource::PARAM_XML_TIMEZONE_ID    => 'Europe/Brussels',
+            XmlDataSource::PARAM_XML_LOCALE_CODE    => 'en_US'
+        ]);
+
+        $reportParams = new ReportParams([
+            'LOGO_FILE'    => \JasperTestsFactories::getReportBaseDir() . '/assets/wave.png',
+            'REPORT_TITLE' => 'PHPUNIT'
+        ]);
+
+        $reportRunner = ReportRunnerFactory::getBridgedReportRunner($this->ba);
+
+        $report = new Report($reportFile, $reportParams, $jsonDataSource);
+
+        $output_pdf = \JasperTestsFactories::getOutputDir() . '/' . basename($reportFile, '.jrxml') . '.pdf';
+        if (file_exists($output_pdf)) {
+            unlink($output_pdf);
+        }
+
+        $exportManager = $reportRunner->getExportManager($report);
+
+        try {
+            $exportManager->savePdf($output_pdf);
+        } catch (JavaProxiedException $e) {
+            //var_dump($e->getJvmStackTrace());
+            throw $e;
+        }
+
+        $pdfUtils = new PDFUtils($output_pdf);
+        $text     = $pdfUtils->getTextContent();
+
+        self::assertContains('Customer Report From XML', $text);
+        self::assertContains('PHPUNIT', $text);
+        self::assertContains('Maria Anders', $text);
+    }
+
+    public function xmlSourceProvider(): array
+    {
+        $xmlFileSource   = \JasperTestsFactories::getDataBaseDir() . '/northwind.xml';
+        $xmlUrlSource    = sprintf(
+            'http://%s:%s/%s',
+            EXPRESSIVE_SERVER_HOST,
+            EXPRESSIVE_SERVER_PORT,
+            'data/northwind-xml'
+        );
+
+        return [
+            [$xmlFileSource], [$xmlUrlSource]
+        ];
+    }
+}
