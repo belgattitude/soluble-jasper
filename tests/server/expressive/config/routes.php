5bc698263f17ba540c4c9bd6e635acb450d13bcd
belgattitude <belgattitude@gmail.com>
2018-03-12 16:18:47 +0100

    Added smoke server data

--- tests/server/expressive/config/routes.php
+++ tests/server/expressive/config/routes.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 use Psr\Container\ContainerInterface;
+use Zend\Diactoros\Response\XmlResponse;
+use Zend\Diactoros\Stream;
 use Zend\Expressive\Application;
 use Zend\Expressive\MiddlewareFactory;
 use Psr\Http\Message\ServerRequestInterface;
@@ -12,10 +14,30 @@ use Zend\Diactoros\Response\JsonResponse;
 
 return function (Application $app, MiddlewareFactory $factory, ContainerInterface $container): void {
     // Test for ping action
+
+
+
     $app->get('/', new class() implements RequestHandlerInterface {
         public function handle(ServerRequestInterface $request): ResponseInterface
         {
             return (new JsonResponse(['success' => true]))->withStatus(200);
+        }
+    });
+
+    $app->get('/data/northwind-json', new class() implements RequestHandlerInterface {
+        public function handle(ServerRequestInterface $request): ResponseInterface
+        {
+            $file = realpath(dirname(dirname(getcwd())) . '/data') . '/northwind.json';
+            return (new JsonResponse([]))->withBody(new Stream($file))->withStatus(200);
+        }
+    });
+
+    $app->get('/data/northwind-xml', new class() implements RequestHandlerInterface {
+        public function handle(ServerRequestInterface $request): ResponseInterface
+        {
+            $file = realpath(dirname(dirname(getcwd())) . '/data') . '/northwind.xml';
+            return (new XMLResponse(''))->withBody(new Stream($file))->withStatus(200);
+
         }
     });
 
