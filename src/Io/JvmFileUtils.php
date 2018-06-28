14aabec378671461c08200022a457de69fa63d47
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-06-28 18:49:38 +0200

    Added utility class for filesystem checks on the JVM side

---
+++ src/Io/JvmFileUtils.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Soluble\Jasper\Io;
+
+use Soluble\Japha\Bridge\Adapter as BridgeAdapter;
+
+class JvmFileUtils
+{
+    /**
+     * @var BridgeAdapter
+     */
+    protected $ba;
+
+    public function __construct(BridgeAdapter $ba)
+    {
+        $this->ba = $ba;
+    }
+
+    /**
+     * @param string $directory
+     *
+     * @throws \Soluble\Japha\Bridge\Exception\ClassNotFoundException
+     * @throws \Soluble\Japha\Bridge\Exception\JavaException
+     */
+    public function isDirectoryWritable(string $directory): bool
+    {
+        try {
+            $jFile = $this->ba->java('java.io.File', $directory);
+        } catch (JavaException $e) {
+            throw $e;
+        }
+
+        if (!$jFile->exists()) {
+            throw new Exception\InvalidDirectoryException(sprintf('Directory \'%s\' does not exists (jvm-side)', $directory));
+        }
+
+        if (!$jFile->isDirectory()) {
+            throw new Exception\InvalidDirectoryException(sprintf('Path \'%s\' is not a directory (jvm-side)', $directory));
+        }
+
+        return $jFile->canWrite();
+    }
+}
