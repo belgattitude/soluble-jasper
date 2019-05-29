0946343cd6ae37c97d323734ba8642f0a091a748
Sébastien Vanvelthem <belgattitude@gmail.com>
2019-05-29 12:16:54 +0200

    copyright update

--- tests/functional/Recipes/BarcodeReportGenerationTest.php
+++ tests/functional/Recipes/BarcodeReportGenerationTest.php
@@ -7,7 +7,7 @@ declare(strict_types=1);
  *
  * @link      https://github.com/belgattitude/soluble-jasper
  * @author    Vanvelthem Sébastien
- * @copyright Copyright (c) 2017 Vanvelthem Sébastien
+ * @copyright Copyright (c) 2017-2019 Vanvelthem Sébastien
  * @license   MIT
  */
 
